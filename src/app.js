d0503e263a87324730c05d69fbd482918da26d45
HursitTarcan <hursittarcan@gmail.com>
2022-06-22 13:35:52 +0200

    Added Views + Installed Pug.js TODO: Proper design for homepage.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Encoding, Content-Transfer-Encoding");
@@ -17,7 +20,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render("home");
 });
 
 const gameRouter = require('./routes/game');
